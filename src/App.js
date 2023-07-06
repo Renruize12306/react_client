31a83d5ef9d9bdefd448a8435fddf8a56ed39fc1
Renruize12306 <renruize1998@gmail.com>
2023-07-06 19:55:10 -0400

    change the private ip address

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <div>neweweweww3001</div> */}
         <div>
         <BrowserRouter>
           <Switch>
